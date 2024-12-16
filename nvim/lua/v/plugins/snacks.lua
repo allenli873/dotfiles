2e52d9c279a6a36bec2eea1f8389a5ed3a9878ac
Allen Li <allenli873@gmail.com>
2024-12-16 01:16:48 -0500

    Update snacks with more snacks features

--- nvim/lua/v/plugins/snacks.lua
+++ nvim/lua/v/plugins/snacks.lua
@@ -3,24 +3,68 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        dashboard = { enabled = true },
+        animate = { enabled = true },
         bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        dim = { enabled = true },
         notifier = { enabled = true },
         quickfile = { enabled = true },
         statuscolumn = { enabled = true },
         words = { enabled = true },
         lazygit = { enabled = true },
+        scroll = { enabled = true },
+        indent = { enabled = true },
     },
     keys = {
+        {
+            "<leader>cR",
+            function()
+                require("snacks").rename.rename_file()
+            end,
+            desc = "Rename File",
+        },
+        {
+            "<leader>gb",
+            function()
+                require("snacks").git.blame_line()
+            end,
+            desc = "Git Blame Line",
+        },
+
+        {
+            "<leader>gf",
+            function()
+                require("snacks").lazygit.log_file()
+            end,
+            desc = "Lazygit Current File History",
+        },
         {
             "<leader>gg",
             function()
                 require("snacks").lazygit()
             end,
             desc = "Lazygit",
+        },
+        {
+            "<leader>gl",
+            function()
+                require("snacks").lazygit.log()
+            end,
+            desc = "Lazygit Log (cwd)",
+        },
+        {
+            "<leader>z",
+            function()
+                require("snacks").zen()
+            end,
+            desc = "Toggle Zen Mode",
+        },
+        {
+            "<leader>Z",
+            function()
+                require("snacks").zen.zoom()
+            end,
+            desc = "Toggle Zoom",
         },
     },
 }
