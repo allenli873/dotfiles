5677282936d033ccd50bf3721bdbc3f70acfe568
Allen Li <allli@twosigma.com>
2025-01-16 22:31:31 -0500

    style: fold snacks keymappings

--- nvim/lua/v/plugins/snacks.lua
+++ nvim/lua/v/plugins/snacks.lua
@@ -15,56 +15,14 @@ return {
         scroll = { enabled = true },
         indent = { enabled = true },
     },
+    -- stylua: ignore
     keys = {
-        {
-            "<leader>cR",
-            function()
-                require("snacks").rename.rename_file()
-            end,
-            desc = "Rename File",
-        },
-        {
-            "<leader>gb",
-            function()
-                require("snacks").git.blame_line()
-            end,
-            desc = "Git Blame Line",
-        },
-
-        {
-            "<leader>gf",
-            function()
-                require("snacks").lazygit.log_file()
-            end,
-            desc = "Lazygit Current File History",
-        },
-        {
-            "<leader>gg",
-            function()
-                require("snacks").lazygit()
-            end,
-            desc = "Lazygit",
-        },
-        {
-            "<leader>gl",
-            function()
-                require("snacks").lazygit.log()
-            end,
-            desc = "Lazygit Log (cwd)",
-        },
-        {
-            "<leader>z",
-            function()
-                require("snacks").zen()
-            end,
-            desc = "Toggle Zen Mode",
-        },
-        {
-            "<leader>Z",
-            function()
-                require("snacks").zen.zoom()
-            end,
-            desc = "Toggle Zoom",
-        },
+        { "<leader>cR", function() require("snacks").rename.rename_file() end, desc = "Rename File" },
+        { "<leader>gb", function() require("snacks").git.blame_line() end, desc = "Git Blame Line" },
+        { "<leader>gf", function() require("snacks").lazygit.log_file() end, desc = "Lazygit Current File History" },
+        { "<leader>gg", function() require("snacks").lazygit() end, desc = "Lazygit" },
+        { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "Lazygit Log (cwd)" },
+        { "<leader>z", function() require("snacks").zen() end, desc = "Toggle Zen Mode" },
+        { "<leader>Z", function() require("snacks").zen.zoom() end, desc = "Toggle Zoom" },
     },
 }
